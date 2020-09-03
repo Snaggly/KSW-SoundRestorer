013631e28fb97ddfa6d7ed4e7d43beff28840a47
Snaggly <oemshani@yahoo.com>
2020-09-04 01:06:46 +0200

    Preload KswBt
    Signed-off-by: Snaggly <oemshani@yahoo.com>

---
+++ app/src/main/java/com/snaggly/ksw_soundrestorer/Helper.java
@@ -0,0 +1,23 @@
+package com.snaggly.ksw_soundrestorer;
+
+import android.app.ActivityManager;
+import android.content.Context;
+
+import java.util.List;
+
+public class Helper {
+
+    public static boolean isAppRunning(final Context context, final String packageName) {
+        final ActivityManager activityManager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        final List<ActivityManager.RunningAppProcessInfo> procInfos = activityManager.getRunningAppProcesses();
+        if (procInfos != null)
+        {
+            for (final ActivityManager.RunningAppProcessInfo processInfo : procInfos) {
+                if (processInfo.processName.equals(packageName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/snaggly/ksw_soundrestorer/MainActivity.java
+++ app/src/main/java/com/snaggly/ksw_soundrestorer/MainActivity.java
@@ -2,15 +2,23 @@ package com.snaggly.ksw_soundrestorer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningAppProcessInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Interpolator;
 import android.media.AudioManager;
+import android.media.Session2Command;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    final private String targetPackage = "com.wits.ksw.bt";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +29,30 @@ public class MainActivity extends AppCompatActivity {
         bigButt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 100, AudioManager.ADJUST_SAME);
-                Toast.makeText(getApplicationContext(), "Sent Command", Toast.LENGTH_SHORT).show();
+                try {
+                    Thread t1 = new Thread(() -> {
+                        try {
+                            Thread.sleep(600);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    t1.start();
+
+                    Intent kswBtIntent = getPackageManager().getLaunchIntentForPackage(targetPackage);
+                    kswBtIntent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
+                    startActivity(kswBtIntent);
+                    t1.join();
+
+                    Intent home = new Intent(Intent.ACTION_MAIN);
+                    home.addCategory(Intent.CATEGORY_HOME);
+                    startActivity(home);
+
+                    finishAndRemoveTask();
+                }
+                catch (Exception e){
+                    Toast.makeText(getApplicationContext(), targetPackage + " could not be started.", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
