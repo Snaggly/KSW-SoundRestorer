b53de326532708a50369bf9d6e5dbf1c6dc102db
Snaggly <oemshani@yahoo.com>
2020-09-05 01:30:53 +0200

    Autorun after Boot finally works!
    Signed-off-by: Snaggly <oemshani@yahoo.com>

--- app/src/main/java/com/snaggly/ksw_soundrestorer/MainActivity.java
+++ app/src/main/java/com/snaggly/ksw_soundrestorer/MainActivity.java
@@ -8,7 +8,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Interpolator;
 import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,12 +21,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     final public static String targetPackage = "com.wits.ksw.bt";
+    final public static int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 5469;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        BtPreloader.preload(this, targetPackage);
+        if (Build.VERSION.SDK_INT >= 23 && !Settings.canDrawOverlays(this)) {
+            startActivityForResult(new Intent("android.settings.action.MANAGE_OVERLAY_PERMISSION", Uri.parse("package:" + getPackageName())), ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+        } else{
+            BtPreloader.preload(this, targetPackage);
+        }
 
         finish();
     }
